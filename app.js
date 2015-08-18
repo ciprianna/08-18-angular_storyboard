f47dec01c18ae065ff4bd1065bcad279fea74761
Ciprianna <ciprianna17@gmail.com>
2015-08-18 13:22:41 -0500

    Added a factory for stories and injected it into the StoryCtrl

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var storyApp = angular.module('storyApp', [])
 
-storyApp.controller('StoryCtrl', function($scope){
-  $scope.stories = [
-    {title: 'Chamber', author: 'JK', upvotes: 5},
-    {title: 'Goblet', author: 'JK', upvotes: 15},
-    {title: 'Prince', author: 'JK', upvotes: 25}
-  ];
-  $scope.addStory = function(){
-    if($scope.title === '' || !$scope.title){return;}
-    $scope.stories.push({title: $scope.title, upvotes: 0})
-    $scope.title = '';
-  };
-  $scope.incrementUpvotes = function(story){
-    story.upvotes += 1;
+storyApp.controller('StoryCtrl', [
+  '$scope',
+  'stories',
+  function($scope, stories){
+    $scope.stories = stories.stories;
+    $scope.addStory = function(){
+      if($scope.title === '' || !$scope.title){return;}
+      $scope.stories.push({title: $scope.title, upvotes: 0})
+      $scope.title = '';
+    };
+    $scope.incrementUpvotes = function(story){
+      story.upvotes += 1;
+    };
+}]);
+
+storyApp.factory('stories', [function(){
+  var storyfuncs = {
+    stories: []
   }
-})
+  return storyfuncs;
+}]);
