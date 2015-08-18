510b57e288a72daa24cdc5cb698071eefd0e9047
Ciprianna <ciprianna17@gmail.com>
2015-08-18 13:59:35 -0500

    Created partials for templates to show home and story views. Created a StoryCtrl for just the story page.

--- app.js
+++ app.js
@@ -7,28 +7,45 @@ storyApp.config([
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: '/home.html',
+        templateUrl: 'home/_home.html',
+        controller: 'TopStoriesCtrl'
+      })
+      .state('stories', {
+        url: '/stories/{id}',
+        templateUrl: 'story/_stories.html',
         controller: 'StoryCtrl'
-      });
-
+      })
     $urlRouteProvider.otherwise('home');
   }
 ])
 
-storyApp.controller('StoryCtrl', [
+storyApp.controller('TopStoriesCtrl', [
   '$scope',
   'stories',
   function($scope, stories){
     $scope.stories = stories.stories;
     $scope.addStory = function(){
       if($scope.title === '' || !$scope.title){return;}
-      $scope.stories.push({title: $scope.title, upvotes: 0})
+      $scope.stories.push({title: $scope.title, upvotes: 0,
+      comments: [
+        {author: 'Cats McGats', body: 'Great story!'},
+        {author: 'Kittens McJittens', body: 'Love this one!'}
+      ]})
       $scope.title = '';
     };
     $scope.incrementUpvotes = function(story){
       story.upvotes += 1;
     };
 }]);
+
+storyApp.controller('StoryCtrl', [
+  '$scope',
+  '$stateParams',
+  'stories',
+  function($scope, $stateParams, stories){
+    $scope.story = stories.stories[$stateParams]
+  }
+]);
 
 storyApp.factory('stories', [function(){
   var storyfuncs = {
