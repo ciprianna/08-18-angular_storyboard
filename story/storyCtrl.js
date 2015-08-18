b182c675944be4cc3431ab7ded42063659724f87
Ciprianna <ciprianna17@gmail.com>
2015-08-18 14:32:46 -0500

    Generated Rails files.

---
+++ story/storyCtrl.js
@@ -0,0 +1,7 @@
+storyApp.controller('StoryCtrl', function($scope){
+  $scope.stories = [
+    {title: 'Chamber', author: 'JK', upvotes: 5},
+    {title: 'Goblet', author: 'JK', upvotes: 15},
+    {title: 'Prince', author: 'JK', upvotes: 25}
+  ]
+})
