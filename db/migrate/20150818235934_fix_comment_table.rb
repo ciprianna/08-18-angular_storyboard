072e5c23db00046f7fe2473c7014b3b4f7336334
Ciprianna <ciprianna17@gmail.com>
2015-08-18 19:03:10 -0500

    Created db structures.

---
+++ db/migrate/20150818235934_fix_comment_table.rb
@@ -0,0 +1,4 @@
+class FixCommentTable < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150818235934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "story_id"
+  end
+
+  add_index "comments", ["story_id"], name: "index_comments_on_story_id", using: :btree
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "upvotes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "comments", "stories"
 end
